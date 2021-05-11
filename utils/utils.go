d50db471ff88e885e117f2f3bf9d3677375cf809
atighineanu <atighineanu@suse.de>
2021-05-11 10:04:49 +0200

    added functionality to check if images are up-to-date

--- utils/utils.go
+++ utils/utils.go
@@ -1,22 +1,27 @@
 package utils
 
 import (
-	"context"	
+	"context"
+	"errors"
 	"fmt"
+	"io"
 	"log"
 	"os"
-	"io"
-	"errors"
+	"os/exec"
+	"regexp"
+	"strings"
+	"time"
+
 	"cloud.google.com/go/storage"
-    "google.golang.org/api/iterator"
+	"google.golang.org/api/iterator"
 	"google.golang.org/api/option"
 )
 
 func CheckIfBucketExists(ctx context.Context, client *storage.Client, bucketName string) {
 	bucket := client.Bucket(bucketName)
-	_,err := bucket.Attrs(ctx)
+	_, err := bucket.Attrs(ctx)
 	if err != nil {
-    log.Fatalf("ERROR: %v",err)
+		log.Fatalf("ERROR: %v", err)
 	} else {
 		log.Println("Bucket exists. OK.")
 	}
@@ -27,46 +32,46 @@ func CheckIfItemExists(jsonPath, projectID, bucketName, fileName string) (bool, 
 	if err != nil {
 		return false, err
 	}
-	var found bool 
-	for _, value := range list {
-		if (fileName == value) {
+	var found bool
+	for index, _ := range list {
+		if fileName == index {
 			found = true
 		}
 	}
 	return found, nil
 }
 
-
-func ListObjectsInBucket(jsonPath, projectID, bucketName, flag string) ([]string, error) {
+func ListObjectsInBucket(jsonPath, projectID, bucketName, flag string) (map[string]time.Time, error) {
 	ctx := context.Background()
 	client, err := storage.NewClient(ctx, option.WithCredentialsFile(jsonPath))
 	if err != nil {
-			return nil, err
+		return nil, err
 	}
 	CheckIfBucketExists(ctx, client, bucketName)
 	//fmt.Printf("%T    %T\n", ctx, client)
 
 	bucket := client.Bucket(bucketName)
+
 	it := bucket.Objects(ctx, nil)
-	
+
 	var i int
-	var itemList []string
-	if (flag != "silent") {
-	fmt.Printf("\n Bucket: %s\n =======================\n", bucketName)
+	itemList := make(map[string]time.Time)
+	if flag != "silent" {
+		fmt.Printf("\n Bucket: %s\n =======================\n", bucketName)
 	}
 	for {
-		i +=1 
+		i += 1
 		attrs, err := it.Next()
 		if err == iterator.Done {
-				break
+			break
 		}
 		if err != nil {
-				return itemList, fmt.Errorf("Bucket(%q).Objects: %v", bucketName, err)
+			return itemList, fmt.Errorf("Bucket(%q).Objects: %v", bucketName, err)
 		}
-		itemList = append(itemList, attrs.Name)
-		if (flag != "silent") {
-			fmt.Printf("	Img_%v: 	%v\n", i, attrs.Name)
-		}	
+		itemList[attrs.Name] = attrs.Created
+		if flag != "silent" {
+			fmt.Printf("	Img_%v: 	%v    Created_at: %v\n", i, attrs.Name, attrs.Created)
+		}
 	}
 	return itemList, nil
 }
@@ -75,7 +80,7 @@ func UploadFile(jsonPath, bucketName, filePath, uploadedFilename string) error {
 	ctx := context.Background()
 	client, err := storage.NewClient(ctx, option.WithCredentialsFile(jsonPath))
 	if err != nil {
-			log.Fatal(err)
+		log.Fatal(err)
 	}
 
 	file, err := os.Open(filePath)
@@ -94,5 +99,63 @@ func UploadFile(jsonPath, bucketName, filePath, uploadedFilename string) error {
 		return err
 	}
 
-	return nil 
+	return nil
+}
+
+func CheckifImgUpdated(imglist map[string]time.Time, downloadSuseLink string) error {
+	cmd := []string{"curl", downloadSuseLink}
+	output, err := exec.Command(cmd[0], cmd[1:]...).CombinedOutput()
+	if err != nil {
+		return err
+	}
+	stroutput := string(fmt.Sprintf("%s", output))
+	if len(imglist) > 0 {
+		for index, value := range imglist {
+			//fmt.Println(value)
+			if strings.Contains(stroutput, index) {
+				log.Printf("%s is the latest Google Cloud Image.\n", index)
+			} else {
+				day, _ := ImgVersioningParser(stroutput, index, "x86_64")
+				if day.Sub(value).Hours() > 3 {
+					log.Printf("There's a newer image for %s. Updating the bucket...\n", index)
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func ImgVersioningParser(webpage string, image string, arch string) (time.Time, error) {
+	tmpwebpageslice := strings.Split(webpage, "Details")
+	imgprefix := strings.Split(image, fmt.Sprintf(".%s-", arch))[0]
+	imgregister := make(map[string][]string)
+	regserver := regexp.MustCompile(".tar.gz")
+	var day time.Time
+	var err error
+	for _, value := range tmpwebpageslice {
+		if regserver.FindString(value) != "" && strings.Contains(value, arch) && strings.Contains(value, "GCE") {
+			if strings.Contains(value, imgprefix) {
+				day, err = ParseWebHTMLLine(value)
+				if err != nil {
+					return day, err
+				}
+				imgregister[imgprefix] = append(imgregister[imgprefix], strings.Split(value, fmt.Sprintf(".%s-", arch))[1])
+			}
+		}
+	}
+	//fmt.Println(imgregister)
+	return day, nil
+}
+
+func ParseWebHTMLLine(htmlLine string) (time.Time, error) {
+	reg := regexp.MustCompile(`\d{2}-\w{3,9}-\d{4} \d{2}:\d{2}`)
+	timestamp := fmt.Sprintf("%s CET", reg.FindStringSubmatch(htmlLine)[0])
+	if strings.Contains(timestamp, "-202") {
+		timestamp = strings.Replace(timestamp, "20", "", 1)
+	}
+	day, err := time.Parse(time.RFC822, strings.Replace(timestamp, "-", " ", 10))
+	if err != nil {
+		return day, err
+	}
+	return day, nil
 }
