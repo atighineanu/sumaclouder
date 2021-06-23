7069d5404e6066faebd097cc65adbe9a820fa539
atighineanu <atighineanu@suse.de>
2021-06-23 23:47:35 +0200

    added imageregister type

--- utils/data.go
+++ utils/data.go
@@ -1,10 +1,17 @@
 package utils
 
+import "time"
+
 type GCEauth struct {
 	IamUser string
 	UID     string
 	Key1    string
 	Key2    string
+}
+
+type ImgRegister struct {
+	Timestamp  time.Time
+	NewImgVers string
 }
 
 type Config struct {
